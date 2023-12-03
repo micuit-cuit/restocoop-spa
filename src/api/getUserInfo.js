8c8dbe9eaa3d904eaaafe52e219449bd1d39ea60
micuit-cuit <micuit-cuit@paire.space>
2023-12-03 13:19:29 +0100

    Refactor getUserInfo.js to use PostgreSQL database

--- src/api/getUserInfo.js
+++ src/api/getUserInfo.js
@@ -1,19 +1,25 @@
-fs = require('fs');
-module.exports.execute = function ({ res, arg, config  }) {
+module.exports.execute = function ({ res, arg, config , userDb}) {
     let { token } = arg[0]
     //test si le token existe dans la base de données
-    let users = fs.readFileSync(config.path.custom.db, 'utf8');
-    users = JSON.parse(users);
-    let user = users.find(u => u.token == token)
-    if (user) {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        let sendUser = {
-            login: user.login,
-            email: user.email,
-        }
-        res.end(JSON.stringify({ message: sendUser, status: true }));
-    } else {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: "Token invalide", status: false }));
-    }
+    const client = userDb
+    client.query(`SELECT * FROM userdb WHERE token = '${token}'`)
+        .then((user) => {
+            user = user.rows
+            if (user.length > 0){ 
+                //renvoie les informations de l'utilisateur (sauf le mot de passe)
+                res.writeHead(200, {'Content-Type': 'text/json'});
+                let {username, email} = user[0]
+                res.write(JSON.stringify({message: {login:username, email}, status: true}));
+                res.end();
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/json'});
+                res.write(JSON.stringify({message: 'token invalide', status: false}));
+                res.end();
+            }
+        })
+        .catch((err) => {
+            res.writeHead(400, {'Content-Type': 'text/json'});
+            res.write(JSON.stringify({message: 'token invalide', status: false}));
+            res.end();
+        })
 }
