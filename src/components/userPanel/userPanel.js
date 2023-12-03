6370b80e6818af87e9b857bdafe1551e3d94463b
micuit-cuit <micuit-cuit@paire.space>
2023-12-03 12:13:16 +0100

    Add styling and functionality to user panel

--- src/components/userPanel/userPanel.js
+++ src/components/userPanel/userPanel.js
@@ -2,11 +2,12 @@ setTimeout(async function(){
     console.log("userPanel");
     //test si l'utilisateur est connecté (un cookie existe)
     let token = getCookie("token");
+    let inioreToken
     console.log(token+" token");
     if (await testToken(token)) {
         console.log("user connected");
         //si l'utilisateur est connecté, on affiche le panel utilisateur
-        document.getElementById("userPanelLink").style.display = "block";
+        document.getElementById("userPanelLink").style.display = "flex";
         document.getElementById("mainLogin").style.display = "none";
     }else{
         console.log("user not connected");
@@ -19,15 +20,17 @@ setTimeout(async function(){
             if (document.getElementById("userPanelLink").style.display == "none"){
                 let token = getCookie("token");
                 console.log(token+" token");
-                if (token != ""){
+                if (token != "" && token != inioreToken){
                     console.log( await testToken(token));
                     if (await testToken(token)) {
                         console.log("user connected");
                         //si l'utilisateur est connecté, on affiche le panel utilisateur
-                        document.getElementById("userPanelLink").style.display = "block";
+                        document.getElementById("userPanelLink").style.display = "flex";
                         document.getElementById("mainLogin").style.display = "none";
                         console.log("clearInterval: "+interval);    
                         clearInterval(interval);
+                    }else{
+                        inioreToken = token
                     }
                 }
             }
